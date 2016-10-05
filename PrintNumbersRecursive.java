d9753415790fdf24d3255f1e35720f087268f8fe
Ami <agianchandani2018@pingry.org>
2016-10-05 19:56:52 -0400

    Full working

--- PrintNumbersRecursive.java
+++ PrintNumbersRecursive.java
@@ -12,54 +12,41 @@ public class PrintNumbersRecursive
 	
 	//printNum(5);
 	
-	//printNumRec(5);
-	//printNumRec(0);
-	
-	pNR(5);
-	pNR(0);
+	printNumRec(5);
+	printNumRec(0);
 	
 	}
 	
-		/**
-		* This method prints the integers from 
-		* 0 to n, both inclusive on separate lines
-		* using recursion
-		* 
-		* @param n the largest integer printed on the screen.
-		*/	
+	/**
+	* This method prints the integers from 
+	* 0 to n, both inclusive on separate lines
+	* not using recursion
+	* @param n the largest integer printed on the screen.
+	*/	
 	
 	public static void printNum(int n)
 	{
 		for (int i = 0; i <= n; i ++)
 		System.out.println(i);
-
 	}
 
 
+	/**
+	* This method prints the integers from 
+	* 0 to n, both inclusive on separate lines
+	* using recursion
+	* if n is greater than 0, it recurses and prints all numbers less than n
+	* until all cases reach the line that prints n
+	* @param n the largest integer printed on the screen.
+	*/	
+
+	}
 	public static void printNumRec (int n)
 	{
-		if (n == 0)
-			System.out.println(0);
-		else
-		{
-		//	System.out.println(' ');
-		//	System.out.println ("testing");
-			printNum(n - 1);
-		}	
-		
-		System.out.println(n);
-
-	}
-	public static void pNR (int n)
-	{
 		if (n > 0)
-			pNR(n - 1);
+			printNumRec(n - 1);
 			
 		System.out.println(n);
 	
 	}
-	
-	
-
-
 }
