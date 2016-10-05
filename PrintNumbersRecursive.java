2e05e1e912ec8887b1f4ddc0aa26d851f0858c9e
Ami <agianchandani2018@pingry.org>
2016-10-05 19:50:20 -0400

    0 working with revisions visible

--- PrintNumbersRecursive.java
+++ PrintNumbersRecursive.java
@@ -12,7 +12,11 @@ public class PrintNumbersRecursive
 	
 	//printNum(5);
 	
-	printNumRec(5);
+	//printNumRec(5);
+	//printNumRec(0);
+	
+	pNR(5);
+	pNR(0);
 	
 	}
 	
@@ -45,8 +49,17 @@ public class PrintNumbersRecursive
 		
 		System.out.println(n);
 
-
 	}
+	public static void pNR (int n)
+	{
+		if (n > 0)
+			pNR(n - 1);
+			
+		System.out.println(n);
+	
+	}
+	
+	
 
 
 }
